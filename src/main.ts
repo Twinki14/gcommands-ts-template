3465dfe2d101827e976946d0be696424816003bf
Twinki <thetwinkehh@gmail.com>
2021-09-08 12:15:30 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,14 +13,14 @@ signale.config({
 const client = new GCommandsClient({
   intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_VOICE_STATES'],
   cmdDir: path.join(__dirname, 'commands'),
-  //eventDir: 'events/',
+  //eventDir: 'path.join(__dirname, 'events')',
   caseSensitiveCommands: false, // true or false | whether to match the commands' caps
   caseSensitivePrefixes: false, // true or false | whether to match the prefix in message commands
   language: 'english', // english, spanish, portuguese, russian, german, czech, slovak, turkish, polish, indonesian, italian
   commands: {
     slash: 'both', // https://gcommands.js.org/docs/#/docs/main/dev/typedef/GCommandsOptionsCommandsSlash
     context: 'both',
-    prefix: '!!', // for normal commands
+    prefix: '!', // for normal commands
   },
   defaultCooldown: '2s',
 });
